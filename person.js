f9884632906a40667760dd90f89a1e999d55cfc4
mespinoza30 <m.espinoza16@hotmail.com>
2021-02-02 10:17:35 -0800

    updated notes

--- person.js
+++ person.js
@@ -1,8 +1,17 @@
 class Person {
+    //in order to get the name/software engineer we need to pass it through the parentheses
+    constructor(name, softwareEngineer) {
+        this.arms = 2;
+        this.legs = 2;
+        this.name = name;
+        this.softwareEngineer = softwareEngineer;
+        this.artists = [];
+    }
     //this is a method
     greet() {
         console.log('Hello SEI-111');
-    } //make sure you do not add commas after the method
+    } 
+    //make sure you do not add commas after the method
     //walk method
     walk() {
         console.log('Walking along the beach.')
@@ -11,12 +20,36 @@ class Person {
     eat(food) {
         console.log(`Today, I am eating ${food}`);
     }
+    //method working with arrays
+    addArtist(artist) {
+        console.log(`I like ${artist}`);
+        this.artists.push(artist)
+    }
 }
 
 //this is an instance of a class
 //known as an object
-const princeLeo = Person();
+//to add name to this person you put it inside the parantheses
+//IMPORTANT: add the new keyword
+const princeLeo = new Person('Leo Brooks', true);
+
+//print attributes
+console.log(princeLeo.name);
+console.log(princeLeo.softwareEngineer);
 
 //calling the methods (invoke)
 princeLeo.walk();
 princeLeo.eat('Dinosaur Chicken Nuggets')
+//a function that we just made for prince leo. this adds a method but not to the template
+//add a method on an instance
+princeLeo.jump = function() {
+    console.log('Weee')
+}
+princeLeo.jump();
+princeLeo.addArtist('Rico Nasty');
+princeLeo.addArtist('Yeye');
+
+//view an attribute
+console.log(princeLeo.artists)
+
+
